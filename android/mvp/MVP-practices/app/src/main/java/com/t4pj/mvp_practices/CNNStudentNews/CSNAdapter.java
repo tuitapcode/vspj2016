027a5d5c625ba98b93baad9cecce3bbb70926ef1
Nguyễn Minh Trí <ngminhtri90@gmail.com>
2016-07-01 03:48:35 +0700

    Update... CNN Student News - search, add

--- android/mvp/MVP-practices/app/src/main/java/com/t4pj/mvp_practices/CNNStudentNews/CSNAdapter.java
+++ android/mvp/MVP-practices/app/src/main/java/com/t4pj/mvp_practices/CNNStudentNews/CSNAdapter.java
@@ -4,10 +4,18 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
 
 import com.t4pj.mvp_practices.R;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.jar.Pack200;
 
 /**
@@ -16,10 +24,53 @@ import java.util.jar.Pack200;
 public class CSNAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private List<Object> items;
+    private List<Object> copyItems;
     private final int CSN_CARD = 0;
+
+    private final static int FADE_DURATION = 1000; // in milliseconds
+    private int lastPos = -1;
+
+    //Copy data khi click search
+    public void setCopyItems(){
+        copyItems.clear();
+        copyItems.addAll(items);
+    }
+
+    //Copy data khi click search
+    public void ResetItems(){
+        items.clear();
+        items.addAll(copyItems);
+        copyItems.clear();
+        notifyDataSetChanged();
+    }
+
+    public void filter(String text) {
+
+        //Phục hồi data
+        if (text.isEmpty()) {
+            items.clear();
+            items.addAll(copyItems);
+        } else {
+            List<Object> result = new ArrayList<>();
+            text = text.toLowerCase();
+            for (Object item: copyItems) {
+                CSNCard i = (CSNCard) item;
+                if (i.getDescription().toLowerCase().contains(text)) {
+                    result.add(item);
+                }
+            }
+
+            items.clear();
+            items.addAll(result);
+        }
+
+        notifyDataSetChanged();
+
+    }
 
     public CSNAdapter(List<Object> items) {
         this.items = items;
+        this.copyItems = new ArrayList<>();
     }
 
     public void AddNew(List<Object> newItems) {
@@ -74,6 +125,36 @@ public class CSNAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
                 break;
         }
 
+//        if (position > lastPos) {
+//            inFromRightAnimation(holder.itemView);
+//            lastPos++;
+//        }
+
+
+    }
+
+    private void setFadeAnimation(View view) {
+        AlphaAnimation anim = new AlphaAnimation(0.0f, 1.0f);
+        anim.setDuration(FADE_DURATION);
+        view.startAnimation(anim);
+    }
+
+    private void setScaleAnimation(View view) {
+        ScaleAnimation anim = new ScaleAnimation(0.5f, 1.0f, 0.5f, 1.0f, Animation.RELATIVE_TO_SELF, 0.75f, Animation.RELATIVE_TO_SELF, 0.75f);
+        anim.setDuration(FADE_DURATION);
+        view.startAnimation(anim);
+    }
+
+    private void inFromRightAnimation(View view) {
+
+        Animation inFromRight = new TranslateAnimation(
+                Animation.RELATIVE_TO_PARENT, +1.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f);
+        inFromRight.setDuration(500);
+        inFromRight.setInterpolator(new AccelerateInterpolator());
+        view.startAnimation(inFromRight);
     }
 
     @Override
--- android/mvp/MVP-practices/app/src/main/java/com/t4pj/mvp_practices/CNNStudentNews/CSNFragment.java
+++ android/mvp/MVP-practices/app/src/main/java/com/t4pj/mvp_practices/CNNStudentNews/CSNFragment.java
@@ -2,16 +2,31 @@ package com.t4pj.mvp_practices.CNNStudentNews;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.t4pj.mvp_practices.R;
 
 import java.util.ArrayList;
+
+import jp.wasabeef.recyclerview.animators.FadeInDownAnimator;
+import jp.wasabeef.recyclerview.animators.FadeInRightAnimator;
+import jp.wasabeef.recyclerview.animators.FadeInUpAnimator;
+import jp.wasabeef.recyclerview.animators.FlipInRightYAnimator;
+import jp.wasabeef.recyclerview.animators.LandingAnimator;
+import jp.wasabeef.recyclerview.animators.SlideInLeftAnimator;
+import jp.wasabeef.recyclerview.animators.SlideInUpAnimator;
 
 /**
  * Load các layout chính, đóng vai trò như 1 view/form
@@ -21,7 +36,11 @@ public class CSNFragment extends Fragment implements CSNConstract.View {
     CSNConstract.Presenter mPresenter;
 
     RecyclerView recyclerView;
+    FloatingActionButton floatingActionButton;
     CSNAdapter csnAdapter;
+
+    int count = 0;
+
 
     public static CSNFragment newInstance() {
         CSNFragment csnFragment = new CSNFragment();
@@ -42,9 +61,9 @@ public class CSNFragment extends Fragment implements CSNConstract.View {
         return items;
     }
 
-    private ArrayList<Object> getSampleArrayList2() {
+    private ArrayList<Object> getSampleArrayList2(String text) {
         ArrayList<Object> items = new ArrayList<>();
-        items.add(new CSNCard(getContext(), "Them~"));
+        items.add(new CSNCard(getContext(), text));
         return items;
     }
 
@@ -52,17 +71,78 @@ public class CSNFragment extends Fragment implements CSNConstract.View {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+        setHasOptionsMenu(true);
         View root = inflater.inflate(R.layout.csn_frag, container, false);
 
         recyclerView = (RecyclerView) root.findViewById(R.id.csn_rv);
+        floatingActionButton = (FloatingActionButton) root.findViewById(R.id.fab);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                csnAdapter.AddNew(getSampleArrayList2("New "+ ++count));
+                recyclerView.scrollToPosition(csnAdapter.getItemCount()-1);
+            }
+        });
+
 
         csnAdapter = new CSNAdapter(getSampleArrayList());
 
+//        recyclerView.setItemAnimator(new SlideInUpAnimator());
         recyclerView.setAdapter(csnAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()));
 
-        csnAdapter.AddNew(getSampleArrayList2());
+//        recyclerView.setItemAnimator(new SlideInLeftAnimator());
+//        recyclerView.getItemAnimator().setAddDuration(500);
+        recyclerView.setItemAnimator(new FadeInUpAnimator());
+//        recyclerView.getItemAnimator().setAddDuration(100);
+
 
         return root;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+        inflater.inflate(R.menu.menu_main, menu);
+        final MenuItem searchItem = menu.findItem(R.id.action_seach);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+
+        searchView.setOnSearchClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                csnAdapter.setCopyItems();
+                floatingActionButton.hide();
+//                Toast.makeText(getContext(), "On Click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                csnAdapter.ResetItems();
+                floatingActionButton.show();
+//                Toast.makeText(getContext(), "On Close", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                csnAdapter.filter(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                csnAdapter.filter(newText);
+                return true;
+            }
+        });
+
+        super.onCreateOptionsMenu(menu, inflater);
     }
 }
